a881a2e65f7d8f901790a37c938051b41ebe3419
Bernard16144 <63929231+Bernard16144@users.noreply.github.com>
2020-04-20 02:23:29 +1200

    Add files via upload

---
+++ signup/register.php
@@ -0,0 +1,49 @@
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>School Management System</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="pic">
+    <center>
+       
+    </center>
+   
+  </div>
+  <div class="header">
+    <img src="passport1.jpg">
+  	<h2>Register</h2>
+  </div>
+	
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Login</a>
+  	</p>
+  </form>
+
+
+</body>
+</html>
